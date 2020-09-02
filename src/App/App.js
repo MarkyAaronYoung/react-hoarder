545b5a38f48cec71cfb866e90022fd30d20d2a18
Mark Young <markyaaronyoung@gmail.com>
2020-09-01 20:15:21 -0500

    added mynavbar & auth components, added helper files, added database

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import fbConnection from '../helpers/data/connection';
+
+import MyNavbar from '../components/pages/MyNavbar/MyNavbar';
+
 import './App.scss';
 
+fbConnection();
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
     return (
       <div className="App">
         <h2>React-Hoarder</h2>
+        <MyNavbar authed={authed}/>
         <button className="btn btn-info">I am a button</button>
       </div>
     );
